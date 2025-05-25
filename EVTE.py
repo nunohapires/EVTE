5e1db5961ac8728a74dddebe0b979d0a435d2365
nunohapires <nuno.pires@ctec.ufal.br>
2025-05-25 20:29:15 -0300

    Update EVTE.py

--- EVTE.py
+++ EVTE.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from scipy import stats
 import plotly.express as px
-import plotly.graph_objects as go
+
 
 # Configuração da página
 st.set_page_config(
